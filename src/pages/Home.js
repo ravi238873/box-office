021df4f8c9d5690ca9b81520ec286e224046a0c5
Ravi <ravi238873@gmail.com>
2022-08-23 10:20:43 +0530

    added api call function

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
 
 const Home = () => {
-  return <MainPageLayout>This is Homepage</MainPageLayout>;
+  const [input, setInput] = useState('');
+
+  const onSearch = () => {
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+      .then(r => r.json())
+      .then(result => {
+        console.log(result);
+      });
+  };
+
+  const onInputChange = ev => {
+    setInput(ev.target.value);
+  };
+
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) {
+      onSearch();
+    }
+  };
+
+  return (
+    <MainPageLayout>
+      <input
+        type="text"
+        onChange={onInputChange}
+        value={input}
+        onKeyDown={onKeyDown}
+      />
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+    </MainPageLayout>
+  );
 };
 
 export default Home;
